459a322911dfe8e90b03c7acebc48153be645d73
tomel1s <tomsin418@gmail.com>
2021-10-03 14:35:15 +0300

    react into laravel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+import React from "react";
+import Example from "./components/Example";
+
+export const App = () => {
+    return (
+        <div>
+            <Example />
+        </div>
+    );
+};
