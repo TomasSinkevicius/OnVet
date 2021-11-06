e8466b07ddd2775834dc8e358f03c5f6f42f2776
tomel1s <tomsin418@gmail.com>
2021-11-06 16:00:18 +0200

    working auth

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Models\Comment;
 use App\Http\Controllers\PostsApiController;
 use App\Http\Controllers\CommentsApiController;
 use App\Http\Controllers\TopicsApiController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,8 +21,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/me', [AuthController::class, 'me']);
 });
 
 
