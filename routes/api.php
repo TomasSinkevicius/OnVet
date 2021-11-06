ee48de1f864add8a8c02ca1bc3d80293229db350
tomel1s <tomsin418@gmail.com>
2021-11-06 13:54:30 +0200

    auth

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);
+});
+
 
 Route::get('/posts', [PostsApiController::class, 'index']);
 
